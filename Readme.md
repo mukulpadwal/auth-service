ea083245cdcd133220f0b78c9f55fd9022d6366c
mukulpadwal <mukulpadwal.me@gmail.com>
2025-08-31 21:29:23 +0530

    complete typeorm setup

---
+++ Readme.md
@@ -0,0 +1,3 @@
+```
+docker run -d -rm --name
+```
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,23 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import dotenv from "dotenv";
+
+const __filename = fileURLToPath(import.meta.url); // Gets the absolute path of the current file
+const __dirname = path.dirname(__filename); // Extracts the directory name from the file path
+
 dotenv.config({
-    path: ".env",
+    path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`),
 });
 
-const { PORT, NODE_ENV } = process.env;
+const { PORT, NODE_ENV, DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME } =
+    process.env;
 
-export const Config = { PORT, NODE_ENV };
+export const Config = {
+    PORT,
+    NODE_ENV,
+    DB_HOST,
+    DB_PORT,
+    DB_USERNAME,
+    DB_PASSWORD,
+    DB_NAME,
+};
