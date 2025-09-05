39bed190552bf04b8a732598b124a41b286903b8
mukulpadwal <mukulpadwal.me@gmail.com>
2025-09-05 15:06:10 +0530

    add email sanitization test

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -212,4 +212,30 @@ describe("POST /auth/register", () => {
             expect(users).toHaveLength(0);
         });
     });
+
+    //
+    describe("Fields are not in proper format.", () => {
+        it("should trim the trailing and leading spaces from the email field", async () => {
+            // Arrange
+            const userData = {
+                firstName: "Mukul",
+                lastName: "Padwal",
+                age: 24,
+                password: "strongpassword",
+                email: "             mukulpadwal.me@gmail.com                ",
+            };
+
+            // Act
+            const response = await request(app)
+                .post("/api/v1/auth/register")
+                .send(userData);
+
+            //Assert
+            const userRepository = connection.getRepository(User);
+            const users = await userRepository.find();
+            const user = users[0];
+
+            expect(user.email).toBe(userData.email.trim());
+        });
+    });
 });
